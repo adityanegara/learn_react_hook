4a24caa06f862a9ee1befc2f0901f20a4e53df7e
unknown <aditnegarabisnis@gmail.com>
2021-02-13 13:52:39 +0700

    Practice : Excercise 1

---
+++ src/component/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Accordion from './Accordion';
+import ExcerciseOne from './ExcerciseOne';
+const items = [
+    {
+        id   : 'item1',
+        title : 'What is React?',
+        content : 'React is a frone end javascript framework'
+    },
+    {
+        id : 'item2',
+        title: 'Why use React?',
+        content : 'Because React is popular among programmer'
+    },
+    {
+        id: 'item3',
+        title: 'How do you use React?',
+        content :  'React use component'
+    }
+];
+
+export default () =>{
+    return(<div className="container mt-3">
+              {/* <Accordion items = {items} test = 'test'/> */}
+              <ExcerciseOne/>
+         </div>);
+}
---
+++ src/component/ExcerciseOne.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const ExcerciseOne = () =>{
+    const [counter, setCounter] = useState(0);
+
+    const onButtonClick = () =>{
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <button onClick = {onButtonClick}>Click Me</button>
+            <h1>{`Current Count : ${counter}`}</h1>
+        </div>
+    )
+
+}
+
+export default ExcerciseOne;
